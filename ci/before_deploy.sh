4cfefbdff7de86fa26ebe8f57554ad0d696f03b6
James Kominick <james.kominick@gmail.com>
2018-01-01 23:25:19 -0500

    update ci scripts

--- ci/before_deploy.sh
+++ ci/before_deploy.sh
@@ -2,33 +2,33 @@
 
 set -ex
 
-main() {
-    #rustup self update
-    #local src=$(pwd) \
-    #      stage=
+# main() {
+#     rustup self update
+#     local src=$(pwd) \
+#           stage=
 
-    #case $TRAVIS_OS_NAME in
-    #    linux)
-    #        stage=$(mktemp -d)
-    #        ;;
-    #    osx)
-    #        stage=$(mktemp -d -t tmp)
-    #        ;;
-    #esac
+#     case $TRAVIS_OS_NAME in
+#         linux)
+#             stage=$(mktemp -d)
+#             ;;
+#         osx)
+#             stage=$(mktemp -d -t tmp)
+#             ;;
+#     esac
 
-    #test -f Cargo.lock || cargo generate-lockfile
+#     test -f Cargo.lock || cargo generate-lockfile
 
-    ## TODO Update this to build the artifacts that matter to you
-    #cross rustc --bin migrant --target $TARGET --release --features 'postgresql sqlite update' -- -C lto
+#     # TODO Update this to build the artifacts that matter to you
+#     cross rustc --bin migrant --target $TARGET --release --features 'postgresql sqlite update' -- -C lto
 
-    ## TODO Update this to package the right artifacts
-    #cp target/$TARGET/release/migrant $stage/
+#     # TODO Update this to package the right artifacts
+#     cp target/$TARGET/release/migrant $stage/
 
-    #cd $stage
-    #tar czf $src/$CRATE_NAME-$TRAVIS_TAG-$TARGET.tar.gz *
-    #cd $src
+#     cd $stage
+#     tar czf $src/$CRATE_NAME-$TRAVIS_TAG-$TARGET.tar.gz *
+#     cd $src
 
-    #rm -rf $stage
-}
+#     rm -rf $stage
+# }
 
 main
