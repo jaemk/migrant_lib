ca5e8f907ba69b1d2f956b67e403cce95ddd5d1f
James Kominick <james.kominick@gmail.com>
2018-01-22 23:51:28 -0500

    test script fix
    - Actually use $MYSQL_PASS
    - Shorten the testing user name
        - mysql complains, must be <= 16 chars

--- test.sh
+++ test.sh
@@ -17,13 +17,13 @@ set -x
 PROJ_DIR=`dirname $0`
 mkdir -p "$PROJ_DIR/db"
 SQLITE=$PROJ_DIR/db/_test_db.db
-POSTGRES=postgres://__migrant_testing:pass@localhost/__migrant_testing
-MYSQL=mysql://__migrant_testing:pass@localhost/__migrant_testing
+POSTGRES=postgres://__migrant_user:pass@localhost/__migrant_testing
+MYSQL=mysql://__migrant_user:pass@localhost/__migrant_testing
 
 
 # Runs a mysql command as root, using a password if provided
 # $1 -> command to execute
-# $2 -> optional mysql root password
+# $2 -> mysql root password
 function do_mysql() {
     if [[ -z $2 ]]; then
         mysql -e "$1"
@@ -32,43 +32,47 @@ function do_mysql() {
     fi
 }
 
+# Setup database tables
+# $1 -> mysql root password
 function setup() {
     touch "$SQLITE"
 
-    sudo -u postgres createuser __migrant_testing
-    sudo -u postgres psql -c "alter user __migrant_testing with password 'pass'"
+    sudo -u postgres createuser __migrant_user
+    sudo -u postgres psql -c "alter user __migrant_user with password 'pass'"
     sudo -u postgres createdb __migrant_testing
 
-    do_mysql "create user '__migrant_testing'@'localhost' identified by 'pass';" $1
+    do_mysql "create user '__migrant_user'@'localhost' identified by 'pass';" $1
     do_mysql "create database __migrant_testing;" $1
-    do_mysql "grant all privileges on __migrant_testing.* to '__migrant_testing'@'localhost';" $1
+    do_mysql "grant all privileges on __migrant_testing.* to '__migrant_user'@'localhost';" $1
 }
 
+# Destroy database tables
+# $1 -> mysql root password
 function teardown() {
     rm "$SQLITE"
 
     sudo -u postgres dropdb __migrant_testing
-    sudo -u postgres psql -c 'drop user __migrant_testing'
+    sudo -u postgres psql -c 'drop user __migrant_user'
 
-    do_mysql "drop user '__migrant_testing'@'localhost';" $1
+    do_mysql "drop user '__migrant_user'@'localhost';" $1
     do_mysql "drop database __migrant_testing;" $1
 }
 
-setup $1
+setup $MYSQL_PASS
 SQLITE_TEST_CONN_STR=$SQLITE POSTGRES_TEST_CONN_STR=$POSTGRES MYSQL_TEST_CONN_STR=$MYSQL cargo test -- --nocapture
 if [ $? -eq 0 ]; then
-    teardown $1
+    teardown $MYSQL_PASS
 else
-    teardown $1
+    teardown $MYSQL_PASS
     exit 1
 fi
 
-setup $1
+setup $MYSQL_PASS
 SQLITE_TEST_CONN_STR=$SQLITE POSTGRES_TEST_CONN_STR=$POSTGRES MYSQL_TEST_CONN_STR=$MYSQL cargo test --features 'sqlite postgresql with-mysql' -- --nocapture
 if [ $? -eq 0 ]; then
-    teardown $1
+    teardown $MYSQL_PASS
 else
-    teardown $1
+    teardown $MYSQL_PASS
     exit 1
 fi
 
